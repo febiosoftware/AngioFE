f91c0c103089ee31a14f59a3494b94bb75b4d850
stevenlabelle <labellestevena@gmail.com>
2021-07-07 03:28:23 +0000

    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Plugins/AngioFE3@13564 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- AngioFE3/PositionDependentDirection.h
+++ AngioFE3/PositionDependentDirection.h
@@ -91,6 +91,7 @@ public:
 	DECLARE_FECORE_CLASS();
 private:
 	bool alpha_override = true;// replace alpha with the override
+	double contribution = 1.0;
 };
 
 class LaGrangePStrainPDD : public PositionDependentDirection
