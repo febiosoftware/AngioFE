0c6bcf0831356d6d3c8a79fa8b4de37504b1841f
stevenlabelle <labellestevena@gmail.com>
2022-03-07 04:37:10 +0000

    Removing dt
    git-svn-id: https://gforge.sci.utah.edu/svn/MRLProjects/Plugins/AngioFE3@13629 b505bdfe-9d5c-0410-8600-697d5fc8e487

--- AngioFE3/ContributionMix.cpp
+++ AngioFE3/ContributionMix.cpp
@@ -17,7 +17,7 @@ struct EigComp {
 
 double PSCPDDContributionMix::ApplyModifiers(double dt, AngioElement* angio_element, vec3d local_pos, FEMesh* mesh)
 {
-		return psc_weight * dt;
+		return psc_weight;
 }
 
 // this is supposed to update the psc to a load curve? Probably also will allow it to be updated by fractional anisotropy, concentrations, gradients, etc.
