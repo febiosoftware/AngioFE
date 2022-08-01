165d294e743eaad3fe6564829185dce76b5fa622
Steven LaBelle <labellestevena@gmail.com>
2022-08-01 15:56:41 -0600

    still good
    still good

--- AngioFE/Fileout.cpp
+++ AngioFE/Fileout.cpp
@@ -10,18 +10,31 @@
 #include "FEAngioMaterial.h"
 #include <unordered_set>
 #include <FECore/FEAnalysis.h>
+#include <FECore/FSPath.h>
+#include <FEBioLib/FEBioModel.h>
+#include <FEBioLib/FEBioConfig.h>
+#include <FEBioLib/febiolib_api.h>
+
+// needed for GetInputFileName()
+#pragma comment(lib, "psapi.lib")
 
 using namespace std;
+
+string Fileout::m_sfile;
 
 //-----------------------------------------------------------------------------
 Fileout::Fileout(FEAngio& angio)
 {
-    logstream.open("out_log.csv");
+	m_sfile = angio.m_fem->GetInputFileName();
+	size_t dot = m_sfile.rfind('.');
+	m_sfile = m_sfile.substr(0, dot);
+
+	logstream.open(m_sfile + "_log.csv");
 	//write the headers
 	logstream << "Time,Material,Segments,Total Length,Vessels,Branches,Anastamoses,Active Tips" << endl;
 
 	// write the line file
-	vessel_state_stream = fopen("out_vess_state.ang2" , "wb");//check the parameters consider setting the compression level
+	vessel_state_stream = fopen((m_sfile + ".ang2").c_str(), "wb");//check the parameters consider setting the compression level
 	// initialize version and line numbers
 	unsigned int magic = 0xfdb97531;
 	unsigned int version = 1;
@@ -51,10 +64,10 @@ Fileout::Fileout(FEAngio& angio)
 		fwrite(&c_bitmask, sizeof(unsigned int), 1, vessel_state_stream);
 	}
 
-	feangio_state_stream = fopen("angio_stats.csv", "wt");
+	feangio_state_stream = fopen((m_sfile + "_time_stats.csv").c_str(), "wt");
 	fprintf(feangio_state_stream, "%-64s,%-64s,%-64s,%-64s\n",
-		"Timestep", "Growth Process","Branch Policy Update", "Update Stress");
-	cell_state_stream = fopen("final_cells.txt", "wt"); // cells
+		"Timestep", "Growth Process", "Branch Policy Update", "Update Stress");
+	cell_state_stream = fopen((m_sfile + "_cells.txt").c_str(), "wt"); // cells
 }
 
 //-----------------------------------------------------------------------------
@@ -199,7 +212,7 @@ void Fileout::save_timeline(FEAngio& angio)
 
 void Fileout::save_final_vessel_csv(FEAngio & angio)
 {
-	FILE * final_vessel_file = fopen("final_vessels.csv", "wt");
+	FILE* final_vessel_file = fopen((m_sfile + "_vessels.csv").c_str(), "wt");
 	assert(final_vessel_file);
 	fprintf(final_vessel_file, "x0,y0,z0,x1,y1,z1,start time\n");
 	FEMesh * mesh = angio.GetMesh();
@@ -219,7 +232,7 @@ void Fileout::save_final_vessel_csv(FEAngio & angio)
 
 void Fileout::save_final_cells_txt(FEAngio & angio)
 {
-	FILE * final_cell_file = fopen("final_cells.txt", "at");
+	FILE* final_cell_file = fopen((m_sfile + "_cells.txt").c_str(), "at");
 	assert(final_cell_file);
 	FEMesh * mesh = angio.GetMesh(); 
 	for (int i = 0; i < angio.angio_elements.size(); i++)
@@ -248,7 +261,7 @@ void Fileout::save_final_cells_txt(FEAngio & angio)
 
 void Fileout::save_initial_cells_txt(FEAngio & angio)
 {
-	FILE * final_cell_file = fopen("final_cells.txt", "at");
+	FILE* final_cell_file = fopen((m_sfile + "_cells.txt").c_str(), "at");
 	assert(final_cell_file);
 	FEMesh * mesh = angio.GetMesh();
 	fprintf(final_cell_file,"*timestep,cell id,X Pos,Y Pos,Z Pos");
